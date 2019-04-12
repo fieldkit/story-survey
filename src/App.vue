14e448ebe0f241989a12c62e959edb284521b92d
Jer Thorp <blprnt@blprnt.com>
2019-04-12 19:24:46 -0400

    Adding files.

---
+++ src/App.vue
@@ -0,0 +1,185 @@
+<template>
+  <div id="app">
+    <div id="imageStack">
+      <img id="base" alt="Fieldkit Landscape" src="./assets/Fieldkit_Panel 01_Final_no_overlays.png" />
+      <img id="clouds" alt="Fieldkit Landscape" src="./assets/Fieldkit_Panel 01_Final_clouds.png" />
+      <img id="signal1" alt="Fieldkit Landscape" src="./assets/Fieldkit_Panel 01_Final_signal1.png" />
+      <img id="signal2" alt="Fieldkit Landscape" src="./assets/Fieldkit_Panel 01_Final_signal2.png" />
+      <img id="signal3" alt="Fieldkit Landscape" src="./assets/Fieldkit_Panel 01_Final_signal3.png" />
+    </div>
+    <HelloWorld msg="Welcome to Fieldkit." msg2="Fieldkit lets everyone everywhere monitor the world around them with low-cost, reliable sensors and easy-to-use tools for storing, sharing and telling stories with data."/>
+    <img alt="Fieldkit Easy Install" src="./assets/Fieldkit_Panel 02_Final_sized.jpg" />
+    <HelloWorld msg="Easy-to-use environmental sensing." msg2="Fieldkit sensors are designed to be accurate, durable and extensible. Our mobile app makes configuration, testing and deployment easy for amateurs and professionals alike."/>
+    <img alt="Fieldkit Easy Install" src="./assets/Fieldkit_Panel 03_Final_sized.jpg" />
+    <HelloWorld msg="For sensor projects, big and small." msg2="Fieldkit’s low per-unit cost makes deploying networks of several, dozens, even hundreds of sensors possible - no matter how small the budget."/>
+    <img alt="Fieldkit Easy Install" src="./assets/Fieldkit_Panel 04_Final_sized.jpg" />
+    <HelloWorld msg="Easily explore and share your data." msg2="The fieldkit platform securely stores and manages your data, and lets you share and tell stories with it."/>
+    <img alt="Fieldkit is for everyone" src="./assets/Fieldkit_Panel 05_Final_sized.png" />
+    <HelloWorld msg="Fieldkit is for everyone." msg2="We’re building fieldkit because we believe everyone should be able to understand and advocate for the world they live in.
+
+    Fieldkit is a tool for field scientists, environmental advocates, naturalists, students and teachers, and most importantly… it’s for you!
+    "/>
+     <img id="campers" alt="Fieldkit Easy Install" src="./assets/Fieldkit_People_01.png" />
+    <HelloWorld msg="We have some questions. Will you help out?" msg2="We've been busy in the lab making first public versions of the Fieldkit sensors + software platform. We're trying to learn a bit more about how people might use Fieldkit in the real world. If you can help us to answer a few easy questions (it should only take three or four minutes), we'll give you a special discount when Fieldkit is released."/>
+    <RoleForm msg="What role best describes what you do?"/>
+    <img id="user" alt="Fieldkit Easy Install" src="./assets/Fieldkit_People_02.png" />
+    <SensorForm msg="What types of sensors are most interesting to you?"/>
+    <img id="sensors" alt="Fieldkit Easy Install" src="./assets/Fieldkit_Panel 01_Final_sensors.png" />
+    <Chooser msg="As a prospective Fieldkit user, which of these features would be more important to you?" right="Low per-unit cost" left="Long battery life" v-on:choose="choose()" total=5 />
+    <img id="contact" alt="Fieldkit Easy Install" src="./assets/Fieldkit_People_03.png" />
+    <Contact msg="Can we stay in touch?"/>
+
+
+
+  </div>
+
+  
+
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import RoleForm from "./components/RoleForm.vue"
+import SensorForm from "./components/SensorForm.vue"
+import Chooser from "./components/Chooser.vue"
+import Contact from "./components/Contact.vue"
+
+const features = [
+  "Low per-unit cost",
+  "Long battery life in the field",
+  "Easy sharing of data",
+  "Data visualization",
+  "Integration with social media feeds",
+  "Research-grade data",
+  "Easy setup for non experts",
+  "An active online community",
+  "Integration with non-Fieldkit sensors"
+]
+
+
+var cloudX = 0;
+
+var tas = [1.0,1.0,1.0];
+
+setInterval( function() {
+  document.getElementById('clouds').style.left = cloudX + "px";
+  //console.log(Math.floor(cloudX) + "px");
+  cloudX += 0.1;
+}, 30);
+
+setInterval( function() {
+
+  var v1 = document.getElementById('signal1').style.opacity;
+  if (v1 == "") v1 = 0;
+  var t1 = parseFloat(v1) + ((tas[0] - parseFloat(v1)) * 0.01);
+  document.getElementById('signal1').style.opacity = t1;
+
+  var v2 = document.getElementById('signal2').style.opacity;
+  if (v2 == "") v2 = 0;
+  var t2 = parseFloat(v2) + ((tas[1] - parseFloat(v2)) * 0.01);
+  document.getElementById('signal2').style.opacity = t2;
+
+  var v3 = document.getElementById('signal3').style.opacity;
+  if (v3 == "") v1 = 0;
+  var t3 = parseFloat(v3) + ((tas[2] - parseFloat(v3)) * 0.01);
+  document.getElementById('signal1').style.opacity = t3;
+
+
+ 
+
+  var chance = 0.005;
+  if (Math.random() < chance) {
+    tas[0] = (tas[0] == 1) ? 0:1;
+    console.log(tas);
+  }
+
+  if (Math.random() < chance) {
+    tas[1] = (tas[1] == 1) ? 0:1;
+  }
+
+  if (Math.random() < chance) {
+    tas[2] = (tas[2] == 1) ? 0:1;
+  }
+
+
+}, 3);
+
+function cloudMove() {
+  
+}
+
+function switchChoose() {
+  alert('test');
+}
+
+function pickTwo() {
+  var i1 = Math.floor(Math.random() * (features.length - 1));
+  var i2 = i1 + Math.ceil(Math.random() * (features.length - 1 - i1));
+  return([i1, i2]);
+}
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld, RoleForm, Chooser, Contact, SensorForm
+  },
+  methods: {
+    choose: function(targ) {
+      var picks = pickTwo();
+      this.$children[7].left = features[picks[0]];
+      this.$children[7].right = features[picks[1]];
+      this.$children[7].total --;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  margin-left:15%;
+  margin-right:15%;
+}
+
+img {
+  max-width:100%;
+}
+
+#imageStack {
+  position:relative;
+}
+
+#clouds, #signal1, #signal2, #signal3 {
+  visibility: visible;
+  position:absolute;
+  left:0px;
+  top:0px;
+
+}
+
+#clouds {
+  z-index:1;
+}
+
+#campers {
+  width:70%;
+}
+
+#user {
+  width:50%;
+}
+
+#contact {
+  width:50%;
+}
+
+p {
+  font-size:1.5em;
+  text-align: left;
+}
+</style>
---
+++ src/components/RoleForm.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id='roleChoose'>
+  <h1>{{ msg }}</h1>
+  <h3>(You can choose more than one!)</h3>
+  <div id='chooseList'>
+
+  <div class="choice">
+  <input type="checkbox" id="fieldScience" value="Field Scientist" v-model="checkedNames">
+  <label for="fieldScience">Field Scientist</label>
+  </div>
+
+  <div class="choice">
+  <input type="checkbox" id="citScience" value="Citizen Scientist" v-model="checkedNames">
+  <label for="citScience">Citizen Scientist</label>
+  </div>
+
+  <div class="choice">
+  <input type="checkbox" id="sciComm" value="Science Comminicator" v-model="checkedNames">
+  <label for="sciComm">Science Communicator</label>
+  </div>
+
+  <div class="choice">
+   <input type="checkbox" id="naturalist" value="Naturalist" v-model="checkedNames">
+  <label for="fieldScience">Naturalist</label>
+  </div>
+
+  <div class="choice">
+  <input type="checkbox" id="enviroAdvocate" value="Environmental Advocate" v-model="checkedNames">
+  <label for="citScience">Environmental Advocate</label>
+  </div>
+
+  <div class="choice">
+  <input type="checkbox" id="engineer" value="Engineer" v-model="checkedNames">
+  <label for="sciComm">Engineer</label>
+  </div>
+  </div>
+  
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "RoleForm",
+  props: {
+    msg:String
+  },
+  data: {
+    checkedNames: []
+  }
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+#roleChoose {
+  margin-top:100px;
+
+}
+
+#chooseList {
+  width:350px;
+  padding-top:25px;
+  margin-left:30%;
+}
+  
+p {
+  text-align:center;
+}
+
+.choice {
+  display:block;
+  text-align:left;
+
+}
+
+label {
+  font-size:18pt;
+  padding-left:10px;
+  text-align:right;
+  padding-right:15px;
+  width:270px;
+}
+
+input[type="checkbox"]{
+  width: 30px; /*Desired width*/
+  height: 30px; /*Desired height*/
+  cursor: pointer;
+  zoom:1.1;
+}
+
+</style>
